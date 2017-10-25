8ad499dcf231a5b4532dcef8c8d63f48880ed8b4
Oscar Magnusson <balobba@gmail.com>
2017-10-25 13:34:29 +0200

    Add TNT sprite

--- src/preload.js
+++ src/preload.js
@@ -2,12 +2,13 @@ function preloadState(game){
     game.load.image('swap', 'assets/powerup_swap.png');
     game.load.image('boost', 'assets/powerup_boost.png');
     game.load.image('shockwave', 'assets/powerup_shockwave.png');
+    game.load.image('shockwave', 'assets/powerup_tnt.png');
 
     game.load.image('player0', 'assets/player0.png');
     game.load.image('player1', 'assets/player1.png');
     game.load.image('player2', 'assets/player2.png');
     game.load.image('player3', 'assets/player3.png');
-    
+
     game.load.image('box', 'assets/box.png');
     game.load.image('HUD', 'assets/HUD.png');
 }
