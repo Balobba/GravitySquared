ec6ae5323d01646b47900b3e351325fa16705cab
Oscar Johansson <1994.oscar.j@gmail.com>
2017-10-25 10:50:48 +0200

    Add random generated maps

--- src/create.js
+++ src/create.js
@@ -3,7 +3,6 @@ function createState(game){
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
 
-  game.tickCounter = 0;
   //Groups
   game.playerGroup = game.add.group();
   game.boxGroup = game.add.group();
@@ -17,8 +16,9 @@ function createState(game){
   createPlayer(game, 100, 350, constGravity, directionEnum.DOWN);
   createPlayer(game, 464, 100, constGravity, directionEnum.UP);
 
-  for(var i = 0; i < 20; i++) {
-    createBox(game, 700-i*30,450, 'box');
+  for(var i = 0; i < game.width/32+5; i++) {
+    createBox(game, i*32,0, 'box');
+    createBox(game, i*32,game.height-32, 'box');
   }
 
   game.border = game.add.sprite(game.width,0);
@@ -26,7 +26,8 @@ function createState(game){
   game.physics.arcade.enable(game.border);
   game.border.body.setSize(10,game.height, 0 ,0);
   game.border.body.immovable = true;
-  createBox(game, 500, 450-32, 'box');
+  game.downY = 0;
+  game.downX = 0;
 
 }
 
@@ -38,6 +39,7 @@ function createBox(game, x, y, name){
   box.body.immovable = true;
   box.body.velocity.x = -200;
   box.body.friction.y = 0;
+  game.lastBlock = box;
   game.boxGroup.add(box);
 }
 
@@ -52,13 +54,10 @@ function createPlayer(game, x, y, gravity, dir){
 
   setGravity(player);
 
-  //key (spacebar)
   player.keyG = game.input.keyboard.addKey(keyBindings[game.playerGroup.length].g);
   player.keyW = game.input.keyboard.addKey(keyBindings[game.playerGroup.length].w);
 
   player.keyG.onDown.add(function(){changeGravity(player)}, this);
-
-
 
   game.playerGroup.add(player);
 
--- src/update.js
+++ src/update.js
@@ -15,16 +15,44 @@ function updateState(game){
       p.keyG.onDown.removeAll();
       p.keyW.onDown.removeAll();
       p.destroy();
-      
     }
   });
 
-    game.tickCounter++;
-    if(game.tickCounter >= 4){
-      game.tickCounter = 0;
-      createBox(game, 830,450, 'box');
-      createBox(game, 830,50, 'box');
+    if(!game.lastBlock || game.lastBlock.body.x-4 < game.width){
+      var randX = Math.random();
+      var randY = Math.random();
+      if(randX < 0.7) {
+      } else if(randX < 0.85){
+        game.downX--;
+        game.downX = Math.max(game.downX, 1);
+      } else {
+        game.downX++;
+      }
+      if(randY < 0.7) {
+      } else if(randY < 0.85){
+        game.downY--;
+        game.downY = Math.max(game.downY, 1);
+      } else {
+        game.downY++;
+      }
+      if(game.downY + game.downX > game.height/32 - 2) {
+        if(game.downY > game.downX) {
+          game.downY -= (game.downY + game.downX) - (game.height/32 - 2);
+        } else {
+          game.downX -= (game.downY + game.downX) - (game.height/32 - 2);
+        }
+      }
+      for(var i = 0; i < game.downX; i++) {
+        createBox(game, 830,i*32, 'box');
+      }
+      for(var i = 0; i < game.downY; i++) {
+        createBox(game, 830,game.height-i*32-32, 'box');
+      }
+
+
+
     }
+
     game.boxGroup.forEach(function(b){
       if(b.body.x < -32) {
         b.destroy();
